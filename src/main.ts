49a2c34b4d103751bc50a2c73b17b9f3d6cc6f9c
jeton <kreatx23>
2021-06-27 17:27:26 +0200

    change module import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { NestApplication, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
 import { Logger } from '@nestjs/common';
@@ -8,23 +8,21 @@ async function bootstrap() {
   console.log(`serverConfig: ${JSON.stringify(serverConfig)}`);
 
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule).catch(error =>
-    console.log(error),
-  );
+  const app = await NestFactory.create(AppModule);
 
-  // if (process.env.NODE_ENV === 'development') {
-  //   app.enableCors();
-  // } else {
-  //   app.enableCors({ origin: serverConfig.origin });
-  //   logger.log(`Accepting requests from "${serverConfig.origin}"`);
-  // }
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from "${serverConfig.origin}"`);
+  }
 
-  // const PORT = process.env.PORT || serverConfig.port;
-  // console.log(`port: ${PORT}`);
-  // console.log(`process.env.PORT: ${process.env.PORT}`);
+  const PORT = process.env.PORT || serverConfig.port;
+  console.log(`port: ${PORT}`);
+  console.log(`process.env.PORT: ${process.env.PORT}`);
 
-  // await app.listen(PORT);
-  // logger.log(`Application listening on port ${PORT}`);
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`);
 }
 
 bootstrap();
