01899d463479c0502a706785e4353bd86c29277b
kukalajet <jeton.kukalaj@gmail.com>
2021-06-29 23:22:19 +0200

    add back devDependencies (removed from dependencies) add old strategy for entities

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,21 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as config from 'config';
-import { Color } from '../colors';
-import { Fuel } from '../fuels';
-import { Listing } from '../listings';
-import { Location } from '../locations';
-import { Manufacturer } from '../manufacturers';
-import { Model } from '../models';
-import { Plate } from '../plates';
-import { Transmission } from '../transmissions';
-import { Valute } from '../valutes';
-import { VehicleType } from '../vehicle-types';
-import { User } from '../users';
 
 const dbConfig = config.get('db');
-
-const entities = [__dirname + '/../**/*.entity.{js,ts}'];
-console.log(entities);
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
@@ -24,20 +10,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.RDS_USERNAME || dbConfig.username,
   password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DB_NAME || dbConfig.database,
-  // entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  entities: [
-    User,
-    Color,
-    Fuel,
-    Listing,
-    Location,
-    Manufacturer,
-    Model,
-    Plate,
-    Transmission,
-    Valute,
-    VehicleType,
-  ],
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
   ssl: {
     rejectUnauthorized: false,
